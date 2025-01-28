2234a7b7fb40725ad6fbfae3550594bce767589c
nusima-59487 <wongktoliver@gmail.com>
2025-01-27 22:24:10 -0800

    Initialization

---
+++ src/main/kotlin/me/nusimucat/roadmap/Roadmap.kt
@@ -0,0 +1,14 @@
+package me.nusimucat.roadmap
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Roadmap : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
