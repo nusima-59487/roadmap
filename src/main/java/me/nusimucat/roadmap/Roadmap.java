0ad78f730543ec28f77193295f8cdac18ec0efc2
nusima-59487 <wongktoliver@gmail.com>
2025-02-21 19:30:34 -0800

    Better logging system

--- src/main/java/me/nusimucat/roadmap/Roadmap.java
+++ src/main/java/me/nusimucat/roadmap/Roadmap.java
@@ -1,35 +1,44 @@
 package me.nusimucat.roadmap;
 
 import java.sql.SQLException;
-import java.util.logging.Level;
 import me.nusimucat.roadmap.commands.MainCommand;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import me.nusimucat.roadmap.database.DBConnect;
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 
 public class Roadmap extends JavaPlugin {
     private static Roadmap pluginInstance;
+    private static ComponentLogger loggerInstance; 
 
     @Override
     public void onEnable () {
         // Plugin startup logic
 
         pluginInstance = this; 
+        loggerInstance = pluginInstance.getComponentLogger(); 
         saveDefaultConfig();
 
         // Establish database connection
         try {
             new DBConnect().sqlConnect();
+            loggerInstance.info("Database connected!"); 
         } 
         catch (SQLException err) {
-            getServer().getLogger().log(Level.WARNING, "Cannot connect to database! Please make sure details in config.yml is correct.");
-            getServer().getLogger().log(Level.WARNING, err.getMessage()); 
+            loggerInstance.warn("Cannot connect to database! Please make sure details in config.yml is correct.");
+            loggerInstance.warn(err.getMessage()); 
             for (StackTraceElement element : err.getStackTrace())
-                getServer().getLogger().log(Level.WARNING, element.toString());
-            getServer().getLogger().log(Level.SEVERE, "Disabling plugin due to above error"); 
-        } 
+                loggerInstance.warn(element.toString());
+            loggerInstance.error("Disabling plugin due to above error"); 
+
+
+            getServer().getPluginManager().disablePlugin(this);
+            
+            
+        }
+        loggerInstance.error("eeeee");
 
         // Load web server (WIP)
 
@@ -49,5 +58,8 @@ public class Roadmap extends JavaPlugin {
 
     public static Roadmap getInstance () {
         return pluginInstance; 
+    }
+    public static ComponentLogger getLoggerInstance () {
+        return loggerInstance; 
     }
 }
