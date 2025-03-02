122b78a766b1b44ad556beb2339117501d73d3b5
nusima-59487 <wongktoliver@gmail.com>
2025-03-02 09:13:05 -0800

    First implementation of SQLite, .db file to be created

--- src/main/java/me/nusimucat/roadmap/database/DBConnect.java
+++ src/main/java/me/nusimucat/roadmap/database/DBConnect.java
@@ -6,28 +6,37 @@ import me.nusimucat.roadmap.ConfigLoader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
 
 public class DBConnect {
     private static Connection connection = null;
     private static final Roadmap pluginInstance = Roadmap.getInstance();
 
-    public void sqlConnect() throws SQLException
-    {
-        String URL = ConfigLoader.getStringVal("database.path"); 
-        String dbName = ConfigLoader.getStringVal("database.name");
-        String userName = ConfigLoader.getStringVal("database.username");
-        String password = ConfigLoader.getStringVal("database.password");
+    public static void sqlConnect() throws SQLException {
+        String storageMethod = ConfigLoader.getStringVal("storage-method");
+        // MySQL
+        if (storageMethod.equalsIgnoreCase("mysql")) {
+            String URL = ConfigLoader.getStringVal("database.path"); 
+            String dbName = ConfigLoader.getStringVal("database.name");
+            String userName = ConfigLoader.getStringVal("database.username");
+            String password = ConfigLoader.getStringVal("database.password");
+            connection = DriverManager.getConnection("jdbc:mysql://" + URL + "/" + dbName + "?autoReconnect=true", userName, password);
+            Roadmap.getLoggerInstance().info("Database (MySQL - " + dbName + ") loaded successfully!"); 
+            // TODO: new SylvBankDBTasks().createTables();
+        }
+        // SQLite
+        else if (storageMethod.equalsIgnoreCase("sqlite")) {
+            connection = DriverManager.getConnection("jdbc:sqlite:plugins/roadmap/database.db");
+            Roadmap.getLoggerInstance().info("Database (SQLite) loaded successfully!"); 
+        }
+        else {
+            Roadmap.getLoggerInstance().error("Invalid storage method in config.yml");
+            Roadmap.getLoggerInstance().error("Disabling plugin due to above error");
+            pluginInstance.getServer().getPluginManager().disablePlugin(pluginInstance);
+        }
 
-        connection = DriverManager.getConnection("jdbc:mysql://" + URL + "/" + dbName + "?autoReconnect=true", userName, password);
-        pluginInstance.getServer().getLogger().log(Level.FINEST, "Database " + dbName + " loaded" +
-                " successfully!");
-
-        // new SylvBankDBTasks().createTables();
     }
 
-    public static Connection getSQLConnection()
-    {
+    public static Connection getSQLConnection() {
         return connection;
     }
 }
