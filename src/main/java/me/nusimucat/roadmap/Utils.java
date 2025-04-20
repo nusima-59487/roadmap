aa67623630ea6a231cafdb94c4a4acacc78023fd
nusima-59487 <wongktoliver@gmail.com>
2025-04-20 15:37:39 -0700

    Utils class first commit

---
+++ src/main/java/me/nusimucat/roadmap/Utils.java
@@ -0,0 +1,53 @@
+package me.nusimucat.roadmap;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.Map.Entry;
+public class Utils {
+    /**
+     * Read a file and return its content as a string
+     * @param pathToFile Path to file after resources folder
+     * @return String - content of the file
+     */
+    public static String readFileToString(String pathToFile) {
+        String toReturn = null; 
+        try {
+            toReturn = new String(
+                Files.readAllBytes(
+                    Paths.get(Roadmap.getInstance().getDataFolder().toString(), pathToFile)
+                )
+            ); 
+        } catch (IOException err) {
+            Roadmap.getLoggerInstance().warn("Cannot connect to database! Please make sure details in config.yml is correct.");
+            Roadmap.getLoggerInstance().warn(err.getMessage()); 
+            for (StackTraceElement element : err.getStackTrace())
+                Roadmap.getLoggerInstance().warn(element.toString());
+        }
+        return toReturn; 
+    }
+
+
+    public static <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
+        Set<T> keys = new HashSet<T>();
+        for (Entry<T, E> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                keys.add(entry.getKey());
+            }
+        }
+        return keys;
+    }
+
+    public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+        for (Entry<T, E> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+}
